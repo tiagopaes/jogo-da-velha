dd3115bca8c6352f9c5aedf7c91c106a6051a927
tiagopaes <tiagopaes37@gmail.com>
2016-11-02 17:17:36 -0200

    code refactoring

--- functions.js
+++ functions.js
@@ -1,61 +1,26 @@
+//Jogo da velha
 
-
-var turno = true;
-var tab = [true,true,true,true,true,true,true,true,true];
-var win = [0,0,0,0,0,0,0,0,0];
-var gameOver = false;
-var jogadores = [
+var players = [
 	{
-		img: "image-x.png",
-		turno: false,
-		win: 1,
-		ganhou: function(){
-		alert("X ganhou");
-		}
+		value: 1,
+		turn: false,
+		write: "X",
+		won: function(){alert("X Ganhou!");}
 	},
 
 	{
-		img: "image-ball.png",
-		turno: true,
-		win: -1,
-		ganhou: function(){
-		console.log("O ganhou");
-		}
-
+		value: -1,
+		turn: true,
+		write: "O",
+		won: function(){alert("O Ganhou!");}
 	}
 ];
 
-function verificaGanhador(posicao) {
-	var x = posicao[0];
-	var y = posicao[1];
-	var z = posicao[2];
-	var soma = this.win[x]+this.win[y]+this.win[z];
-
-	if (soma== 3 || soma== -3){
-		return true;
-	}
-	return false;
-	}
-		
-function userChoice (user){	
-	if(gameOver){
-		return false;
-	}		
-	var i=(user - 1);
-	var elName = "pos" + user;
-	var jogador = 0;
-	if (tab[i]){
-				
-		if (!turno){
-			jogador = 1;				
-		}
-		document.getElementById(elName).src = jogadores[jogador].img;
-		this.turno = jogadores[jogador].turno;
-		this.win [i] = jogadores[jogador].win;
-
-		this.tab[i]=false;
-	}	
-			
+function Game(players) {
+	var turn = true;
+	var gameOver = false;
+	var board = [true,true,true,true,true,true,true,true,true];
+	var valuePositions = [0,0,0,0,0,0,0,0,0];
 	var winPos = [
 		[0,1,2],
 		[0,4,8],
@@ -66,16 +31,61 @@ function userChoice (user){
 		[1,4,7],
 		[2,5,8]
 	];	
-			
-	for (var i = 0; i <= winPos.length; i++) {
-		//console.log("verificando", i);
-		var ganhou = verificaGanhador(winPos[i]);
-		if(ganhou){
-			//console.log("ganhooooooo");
-			gameOver = true;
-			jogadores[jogador].ganhou();
-			break;
+
+	this.move = function(number){
+		if(gameOver){
+			return false;
 		}
-	}	
+		var player = 0;
+		var id = "position" + number;
+		var position = document.getElementById(id);
+
+		if(board[number]){
+
+			if (!turn){
+				player = 1;				
+			}
+
+			position.innerHTML = players[player].write;
+			turn = players[player].turn;
+			board[number] = false;
+			valuePositions[number] = players[player].value;
+		}
+
+		for (var i = 0; i <= 7; i++) {
+			if(this.checkWinner(winPos[i])){
+				gameOver = true;
+				players[player].won();
+				break;
+			}
+		}
+	}
+
+	this.checkWinner = function(positions){
+		var x = positions[0];
+		var y = positions[1];
+		var z = positions[2];
+		var sum = valuePositions[x]+ valuePositions[y]+ valuePositions[z];
+
+		if (sum== 3 || sum== -3){
+			return true;
+		}
+		return false;
+	}
+
+	this.boardEvent = function(number){
+		var id = "position" + number;
+		var position = document.getElementById(id);
+		position.addEventListener('click', function(){
+		jogoDaVelha.move(number);
+		});
+	} 
 }
 
+//events
+
+var jogoDaVelha = new Game(players);
+
+for (var i = 0; i<=8; i++) {
+	jogoDaVelha.boardEvent(i);	
+}
