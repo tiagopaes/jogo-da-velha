f2fb57caf289abf5ae99b1b03dfe1160ec95deb6
tiagopaes <tiagopaes37@gmail.com>
2016-11-12 17:41:09 -0200

    features restart, draw and scoreBoard

--- functions.js
+++ functions.js
@@ -1,18 +1,30 @@
 //Jogo da velha
 
 var players = [
-	{
+	{	
+		scoreBoard: 0,
 		value: 1,
 		turn: false,
 		write: "X",
-		won: function(){alert("X Ganhou!");}
+		won: function(){
+				document.getElementById('textWon').innerHTML = "Venceu!";
+				document.getElementById('iconWon').innerHTML = "X";
+				document.getElementById('iconWon').style.display = "block";
+				document.getElementById('won').style.display = 'block';
+			}
 	},
 
 	{
+		scoreBoard: 0,
 		value: -1,
 		turn: true,
 		write: "O",
-		won: function(){alert("O Ganhou!");}
+		won: function(){
+				document.getElementById('textWon').innerHTML = "Venceu!";
+				document.getElementById('iconWon').innerHTML = "O";
+				document.getElementById('iconWon').style.display = "block";
+				document.getElementById('won').style.display = 'block';
+			}
 	}
 ];
 
@@ -56,9 +68,12 @@ function Game(players) {
 			if(this.checkWinner(winPos[i])){
 				gameOver = true;
 				players[player].won();
+				players[player].scoreBoard ++;
+				document.getElementById(players[player].write).innerHTML = "" + players[player].scoreBoard;
 				break;
 			}
 		}
+		this.draw();
 	}
 
 	this.checkWinner = function(positions){
@@ -79,13 +94,45 @@ function Game(players) {
 		position.addEventListener('click', function(){
 		jogoDaVelha.move(number);
 		});
-	} 
+	}
+
+	this.restart = function(){
+		gameOver = false;
+		board = [true,true,true,true,true,true,true,true,true];
+		valuePositions = [0,0,0,0,0,0,0,0,0];
+		for(var i = 0; i <= 8; i++){
+			var id = "position" + i;
+			var position = document.getElementById(id);
+			position.innerHTML = "";
+		}
+	}
+
+	this.draw = function(){
+		for(var i=0;i<=8;i++){
+			if(board[i]){
+				console.log('true');
+				return true;
+			}
+		}
+		if(!gameOver){
+			console.log('chamou');
+			document.getElementById('textWon').innerHTML = "Empatou!";
+			document.getElementById('iconWon').style.display = 'none';
+			document.getElementById('won').style.display = 'block';
+		}
+	}
 }
 
 //events
 
 var jogoDaVelha = new Game(players);
+var restartButon = document.getElementById('restart');
 
 for (var i = 0; i<=8; i++) {
 	jogoDaVelha.boardEvent(i);	
 }
+
+restartButon.addEventListener('click', function(){
+	document.getElementById('won').style.display = 'none';
+	jogoDaVelha.restart();
+});
